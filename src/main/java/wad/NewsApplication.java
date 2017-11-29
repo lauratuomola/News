b050a44b6bc9f937ebfd2302b5b0ffeb1a9c91b7
Laura Tuomola <laura.tuomola@helsinki.fi>
2017-11-29 14:24:25 +0200

    githubiin

---
+++ src/main/java/wad/NewsApplication.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wad;
+
+import org.springframework.boot.SpringApplication;
+
+
+public class NewsApplication {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(NewsApplication.class, args);
+    }
+    
+}
