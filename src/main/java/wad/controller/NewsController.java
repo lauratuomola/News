456ea4ec8833592f3c664194995fc305201ea0f5
Laura Tuomola <laura.tuomola@helsinki.fi>
2017-11-29 23:46:41 +0200

    uutinen.html

--- src/main/java/wad/controller/NewsController.java
+++ src/main/java/wad/controller/NewsController.java
@@ -7,15 +7,28 @@ package wad.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class NewsController {
     
-    @GetMapping("*")
+    @GetMapping("/")
     
     public String home() {
         return "index";
+    }
+    
+    @GetMapping("/uutinen")
+    public String uutinen(){
+        return "uutinen";
+    }
+    
+    @PostMapping("/uutinen")
+    public String add(@RequestParam String header){
+        return "redirect:/uutinen";
     }
     
 }
