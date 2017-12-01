801f3914cc7debca4963eab94df3a537408f2af1
Laura Tuomola <laura.tuomola@helsinki.fi>
2017-12-01 14:26:18 +0200

    procfile

--- src/main/java/wad/ProductionConfiguration.java
+++ src/main/java/wad/ProductionConfiguration.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package wad;
 
 import javax.activation.DataSource;
@@ -12,17 +8,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-/**
- *
- * @author lautuomo
- */
+
 @Profile("production")
 @Configuration
 public class ProductionConfiguration {
 
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource")
-    public javax.sql.DataSource dataSource() {
+    public DataSource dataSource() {
         System.out.println("Hello world!");
 
         return DataSourceBuilder.create().build();
