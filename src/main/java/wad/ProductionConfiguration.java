b111485244614eeb94e2df118c5ca9796a1ce87a
Laura Tuomola <laura.tuomola@helsinki.fi>
2017-12-11 16:00:21 +0200

    commented productionConfiguration

--- src/main/java/wad/ProductionConfiguration.java
+++ src/main/java/wad/ProductionConfiguration.java
@@ -1,23 +1,23 @@
 
-package wad;
-
-import javax.activation.DataSource;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-
-
-@Profile("production")
-@Configuration
-public class ProductionConfiguration {
-
-    @Bean
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public javax.sql.DataSource dataSource() {
-        
-
-        return DataSourceBuilder.create().build();
-    }
-}
+//package wad;
+//
+//import javax.activation.DataSource;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.boot.jdbc.DataSourceBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Profile;
+//
+//
+//@Profile("production")
+//@Configuration
+//public class ProductionConfiguration {
+//
+//    @Bean
+//    @ConfigurationProperties(prefix = "spring.datasource")
+//    public javax.sql.DataSource dataSource() {
+//        
+//
+//        return DataSourceBuilder.create().build();
+//    }
+//}
